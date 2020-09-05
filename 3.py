e3ada914cbd2fc223e5b6d9c0ddf539e43788683
Lorenzo Motti <loremotti@yahoo.it>
2020-09-05 14:23:15 +0200

    problems until 9, in progress

--- 3.py
+++ 3.py
@@ -1,3 +1,7 @@
+primes = [2]
+max = 0
+
+
 def check_prime(n: int) -> bool:
     for i in range(2, n):
         if n % i == 0:
@@ -7,8 +11,10 @@ def check_prime(n: int) -> bool:
     return False
 
 
-for i in range(600851475143, 0, -2):
-    if 600851475143 % i == 0:
-        if check_prime(i):
-            print(i)
-            break
+for i in range(3, int(600851475143 / 2), 2):
+    if check_prime(i):
+        print(i)
+        print(primes)
+        primes.append(i)
+        if 600851475143 % i == 0:
+            max = i
